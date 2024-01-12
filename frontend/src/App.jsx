0f563e87a9f37c5bc55f5d27797c324fec00dae0
AD202310 <a.g.dychenko@gmail.com>
2024-01-11 19:41:32 -0500

    Display 3 Different Photos (W07D3.9)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
-
 
 const sampleDataForPhotoListItem = {
   id: "1",
@@ -15,16 +13,22 @@ const sampleDataForPhotoListItem = {
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
 
-
 const App = () => {
+
+  const photos = Array.from({ length: 3 });
+
   return (
     <div className="App">
-
-      <PhotoListItem
-        key={sampleDataForPhotoListItem.id}
-        imageSource={sampleDataForPhotoListItem.imageSource}
+      {photos.map((_, index) => (
+        <PhotoListItem
+          key={index}
+          city={sampleDataForPhotoListItem.location.city}
+          country={sampleDataForPhotoListItem.location.country}
+          imageSource={sampleDataForPhotoListItem.imageSource}
+          username={sampleDataForPhotoListItem.username}
+          profile={sampleDataForPhotoListItem.profile}
         />
-
+      ))}
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,9 +3,17 @@ import "../styles/PhotoListItem.scss";
 
 
 const PhotoListItem = (props) => {
+
   return(
     <div className="photo-list__item">
-      <img src={props.imageSource} />
+      <img className="photo-list__image" src={props.imageSource} />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={props.profile} />
+        <div className="photo-list__user-info">
+          <h4 className="photo-list__user-details">{props.username}</h4>
+          <h4 className="photo-list__user-location" >{props.city}, {props.country}</h4>
+        </div>
+      </div>
     </div>
   )
 };
