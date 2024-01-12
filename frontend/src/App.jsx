60b277a125e31b036cb439e5dbe2212e573e8d95
AD202310 <a.g.dychenko@gmail.com>
2024-01-12 11:29:06 -0500

    Include TopicList and FavBadge into TopNavigationBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import './App.scss';
 import './styles/TopNavigationBar.scss'
 import PhotoList from 'components/PhotoList';
-import TopicList from 'components/TopicList';
 import TopNavigationBar from 'components/TopNavigationBar';
-import FavBadge from 'components/FavBadge';
+
 
 
 const App = () => {
@@ -13,8 +12,6 @@ const App = () => {
     <div className="App">
       <nav className="top-nav-bar">
         <TopNavigationBar />
-        <TopicList />
-        <FavBadge />
       </nav>
       <PhotoList />
     </div>
