f3ad0756dec6923251e245cf64d26d376548cc49
AD202310 <a.g.dychenko@gmail.com>
2024-01-12 10:04:00 -0500

    Create Different Photo Topics (W07D4.2)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 import PhotoList from 'components/PhotoList';
+import TopicList from 'components/TopicList';
+import TopicListItem from 'components/TopicListItem';
 
 
 const App = () => {
 
   return (
     <div className="App">
+      <div>
+        <TopicList />
+      </div>
       <PhotoList />
     </div>
   );
