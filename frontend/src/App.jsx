e121f745057838344b3299f6cd59c62117024799
AD202310 <a.g.dychenko@gmail.com>
2024-01-12 20:18:46 -0500

    Create a Side Peek (W07D5.2)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import './styles/TopNavigationBar.scss'
+import './styles/TopNavigationBar.scss';
 import HomeRoute from 'components/HomeRoute';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import mockPhotoData from 'mocks/photos';
 import mockTopicData from 'mocks/topics';
 
-
-
 const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+
+  const openModal = (photo) => {;
+    setSelectedPhoto(photo);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedPhoto(null);
+    setIsModalOpen(false);
+  };
 
   return (
-    <HomeRoute photos={mockPhotoData} topics={mockTopicData}/>
+    <>
+      <HomeRoute photos={mockPhotoData} topics={mockTopicData} onPhotoClick={openModal} />
+      {isModalOpen && <PhotoDetailsModal photo={selectedPhoto} onClose={closeModal} />}
+    </>
   );
 };
 
