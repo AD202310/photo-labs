264ec0fba6a4cc7992afb5485939d250270e79bb
AD202310 <a.g.dychenko@gmail.com>
2024-01-12 21:26:18 -0500

    And Then, Pass Photo Details (W07D5.4)

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,11 +3,17 @@ import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = ({ photo, onClose }) => {
+  console.log("-----------PhotoDetailsModal: ", photo)
+  
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={onClose}>
+      <button onClick={onClose}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
+      <h1>
+        Photo details: 
+      </h1>
+
     </div>
   );
 };
